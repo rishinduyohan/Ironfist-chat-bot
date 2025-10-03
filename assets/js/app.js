d1d3e9da663b5f9e1414f8e3be5e124e1c7d1758
Rishindu Yohan <rishinduyohan@gmail.com>
2025-10-03 18:44:29 +0530

    Enhance speech functionality with personalized greetings and improve mic button class

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 const mic = document.querySelector('.mic');
-
+const content = document.querySelector('.content');
 function speak(text){
     const text_speak = new SpeechSynthesisUtterance(text);
     text_speak.rate = 1;
@@ -9,6 +9,45 @@ function speak(text){
     window.speechSynthesis.speak(text_speak);
 }
 
+wishMe=()=>{
+    var day = new Date();
+    var hour = day.getHours();
+
+    if(hour>=0 && hour<12){
+        speak("Good Morning Challenger...");
+    }
+    else if(hour>=12 && hour<18){
+        speak("Good Afternoon Master...");
+    }
+    else if(hour>=18 && hour<24){
+        speak("Good Evening Boss...");
+    }
+}
 window.addEventListener('load', ()=>{
     speak("Initializing IronFist..");
+    wishMe();
+});
+
+function takeCommand(){
+    if(MessageChannel.includes('hey') || message.includes('hello')){
+        speak("Hello Sir How may I Help You?");
+    }else{
+        speak("Make more intellegent...")
+    }
+}
+
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+
+recognition.onresult = (event)=>{
+    const currentIndex = event.resultIndex;
+    const transcript = event.results[currentIndex][0].transcript;
+    content.textContent = transcript;
+    takeCommand(textContent.toLowerCase());
+}
+
+btn.addEventListener('click',()=>{
+    content.textContent = "Listning...";
+    recognition.start();
 });
